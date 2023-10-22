fef0b30405eb75016f6da3b3133adc38702bfa16
TearingStraw277 <140272052+TearingStraw277@users.noreply.github.com>
2023-10-22 16:31:19 +0100

    Update to Main screen

--- Tea/App.py
+++ Tea/App.py
@@ -2,22 +2,32 @@ import tkinter as Tk
 import customtkinter as Ctk
 import Motor
 
+class MyTabView(Ctk.CTkTabview):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        # create tabs
+        self.add("Black Tea")
+        self.add("Green Tea")
+
+        # add widgets on tabs
+        self.label = Ctk.CTkLabel(master=self.tab("Black Tea"),text="Select from black teas")
+        self.label.place(x=200, y=0)
+
+        self.Black_tea = Ctk.CTkSegmentedButton(master=self.tab("Black Tea"), values=["Earl Grey","Darjeeling","Assam"],dynamic_resizing=True,command=App.button_click)
+        self.Black_tea.place(x=180,y=40)
+
+
 class App(Ctk.CTk):
 
     def __init__(self):
         super().__init__()
         self.geometry("600x500")
-        self.title("Tea chooser")
+        self.title("Tea Selecter")
         self._set_appearance_mode("dark")
-        self.grid_rowconfigure(5,weight=1)
-        self.grid_columnconfigure(5,weight=1)
 
-        
-        self.Black_tea = Ctk.CTkSegmentedButton(self, values=["Earl Grey","Darjeeling","Assam"],dynamic_resizing=True,command=self.button_click)
-        self.Black_tea.place(x=250,y=10)
-
-        self.Green_tea = Ctk.CTkSegmentedButton(self,values=["Geen tea"],dynamic_resizing=True,command=self.button_click)
-        self.Green_tea.place(x=250,y=40)
+        self.tab_view = MyTabView(master=self,width = 550,height = 450)
+        self.tab_view.place(x=20,y=20)
 
     
     def button_click(self,name):
